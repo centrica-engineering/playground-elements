8d303daa94a27d0fce3dad6661e18abe1d42adb6
Jonathan Holt <jonathan.holt@britishgas.co.uk>
2026-02-06 18:37:37 +0000

    chore: improve drag

--- src/playground-tab-bar.ts
+++ src/playground-tab-bar.ts
@@ -227,7 +227,8 @@ export class PlaygroundTabBar extends PlaygroundConnectedElement {
                       viewBox="0 0 24 24"
                       width="16"
                       height="16"
-                      fill="rgba(0,0,0,.1)"
+                      fill="var(--playground-tab-bar-foreground-color, #000)"
+                      style="opacity: 0.25"
                     >
                       <path
                         d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
